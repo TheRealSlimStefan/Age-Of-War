1781358dab09aa15212bd0684691a40d8ed14a86
TheRealSlimStefan <19smarcins98@gmail.com>
2021-11-18 23:25:21 +0100

    containers

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+//containers
+
+// let app;
+// let titleScreen;
+// let mainScreen;
+// let endScreen;
+
+// app = new PIXI.Application({
+//     width: 800,
+//     height: 600,
+//     backgroundColor: 0xaaaaaa,
+// });
+// document.body.appendChild(app.view);
+
+// titleScreen = new PIXI.Container();
+// mainScreen = new PIXI.Container();
+// endScreen = new PIXI.Container();
+
+// mainScreen.visible = false;
+// endScreen.visible = false;
+
+// app.stage.addChild(titleScreen);
+// app.stage.addChild(mainScreen);
+// app.stage.addChild(endScreen);
+
+// let redRect = new PIXI.Graphics();
+// redRect.beginFill(0xff0000);
+// redRect.drawRect(0, 0, app.view.width, app.view.height);
+// titleScreen.addChild(redRect);
+
+// let greenRect = new PIXI.Graphics();
+// greenRect.beginFill(0x00ff00);
+// greenRect.drawRect(0, 0, app.view.width, app.view.height);
+// mainScreen.addChild(greenRect);
+
+// let blueRect = new PIXI.Graphics();
+// blueRect.beginFill(0x0000ff);
+// blueRect.drawRect(0, 0, app.view.width, app.view.height);
+// endScreen.addChild(blueRect);
+
+// window.addEventListener("keyup", switchContainer);
+
+// function switchContainer(e) {
+//     titleScreen.visible = false;
+//     mainScreen.visible = true;
+// }
+
 //SpriteSheet animated movement
 
 // let keys = {};
